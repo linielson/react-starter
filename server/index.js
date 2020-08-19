12e69f373938b264316e0f37ce300e0b20a05764
Linielson Rosa <linielsonrosa@gmail.com>
2020-08-19 19:54:24 -0300

    Create server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// Section 1
+const express = require('express');
+// Section 2
+const app = express();
+// Section 3
+app.get('/', (req, res) => {
+ res.send("<h1>Home page</h1>");
+});
+// Section 4
+app.listen(3000, () => {
+ console.log('server started on port 3000');
+});
